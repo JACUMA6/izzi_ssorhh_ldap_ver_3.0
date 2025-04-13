a88b71369653008cbe156a962cb597991fd90890
JACUMA6 <J0s3cub432@gmail.com>
2025-04-13 14:29:33 -0600

    Update project

--- mcsv-izzi-empleados/src/main/java/mcsv/izzi/empleados/McsvIzziEmpleadosApplication.java
+++ mcsv-izzi-empleados/src/main/java/mcsv/izzi/empleados/McsvIzziEmpleadosApplication.java
@@ -2,8 +2,10 @@ package mcsv.izzi.empleados;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class McsvIzziEmpleadosApplication {
 
 	public static void main(String[] args) {
